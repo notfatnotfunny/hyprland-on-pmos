cedc7325bdf724958b2b059b93d329f8b4366b2d
notfatnotfunny <165948792+notfatnotfunny@users.noreply.github.com>
2026-04-28 11:09:12 +0200

    update gestures for hyprshot

--- dots/hypr/scripts/gestures.sh
+++ dots/hypr/scripts/gestures.sh
@@ -1,1 +1,1 @@
-lisgd -d /dev/input/by-path/platform-a90000.i2c-event -g '1,DU,B,*,*,kill -SIGRTMIN "$(pgrep wvkbd-mobintl)"' -g "1,UD,T,*,*,hyprshot -m region" 
+lisgd -d /dev/input/by-path/platform-a90000.i2c-event -g '1,DU,B,*,*,kill -SIGRTMIN "$(pgrep wvkbd-mobintl)"' -g "1,UD,T,*,*,hyprshot -m region --clipboard-only" 
